0a890cdf521c59d1926efca4b04b29fd1e19da68
Agathe Mazur <agathe.mazur@outlook.fr>
2022-05-25 19:30:23 +0200

    avancement css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-console.info('Hello world');
+"use strict";
+
+import Swiper from 'swiper';
+
+// const swiper = new Swiper('.swiper-container', {
+//     direction: 'horizontal',
+//     loop: true,
+// });
