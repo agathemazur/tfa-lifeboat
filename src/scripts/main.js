974fd9e9b564f8ec2161fe1c0ba3d0428bd8193a
Agathe Mazur <agathe.mazur@outlook.fr>
2022-05-31 17:29:13 +0200

    ajout slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 "use strict";
 
-import Swiper from 'swiper';
-
-// const swiper = new Swiper('.swiper-container', {
-//     direction: 'horizontal',
-//     loop: true,
-// });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
